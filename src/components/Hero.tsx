3aa0881431c31a2bd443e3f0a6c26fc9ccbf1675
Анна Гащук <annahashchuk@Home-MacBook-Pro.local>
2024-03-25 15:45:48 +0200

    Registration button in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,13 +13,13 @@ export default function Hero() {
         <h3 className="text-7xl md:text-8xl font-bold mb-5">
           BEST Company Insight
         </h3>
-        <p className="flex flex-col text-2xl md:text-4xl font-bold mb-5">
+        <p className="flex flex-col text-2xl md:text-4xl font-bold mb-2">
           <span>Привіт, студенте!</span>
           Час відвідати компанію твоєї мрії настав!
         </p>
       </div>
       <Marquee
-        className="speed-90 py-3 flex gap-6 z-10 mb-8"
+        className="speed-90 py-3 flex gap-6 z-10"
         speed={7}
         autoFill={true}
       >
@@ -39,6 +39,12 @@ export default function Hero() {
             ))}
         </div>
       </Marquee>
+      <a className="mt-3 mb-14 scale-75" href="https://docs.google.com/forms/d/e/1FAIpQLSeV-6rCYsm2e-Bf7Dcb83vBuk85ggH-QP1zaCZYBRBUUWlxAQ/viewform">
+        <button className="font-medium text-bci-gray hover:text-bci-white text-2xl md:text-3xl text-center bg-bci-white py-6 px-14 rounded-2xl hover:scale-110 duration-500 hover:shadow-normal
+            border border-bci-white hover:border-bci-white hover:bg-bci-purple z-20">
+            <p>Зареєструватися</p>
+        </button>
+      </a>
       <Image
         src="/BgElement.svg"
         width={800}
